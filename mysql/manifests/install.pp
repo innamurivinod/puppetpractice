09e8444b9c94e17b6e919f51f547a51b931b876c
innamurivinod <innamurivinod123@gmail.com>
2017-04-09 08:39:06 +0530

    install mysql

---
+++ mysql/manifests/install.pp
@@ -0,0 +1,14 @@
+# Class: mysql::install
+#
+#
+class mysql::install {
+ 
+$pack_name = $facts['os']['family'] ? {
+    'RedHat' => 'mysql'
+    'Debian' => 'mysql-server-5.6'
+}
+package { $pack_name:
+    ensure => installed,
+    
+}
+}
